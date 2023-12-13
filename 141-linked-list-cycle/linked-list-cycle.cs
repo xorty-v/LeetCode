9b9bfc8af9a689a1cbcb4df17eaf98c9f3d4841d
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2023-12-13 04:52:06 +0400

    Time: 88 ms (78.85%) | Memory: 43.2 MB (39.78%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        if(head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(slow != null && fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
