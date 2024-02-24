2f23cbeb129316994273327a8d6af3c65f168c2a
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-24 17:40:54 +0400

    Time: 144 ms (80.38%) | Memory: 78.8 MB (68.65%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) 
+    {
+        var anagrams = new Dictionary<string, IList<string>>();
+        
+        foreach(var str in strs)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Sort(chars);
+            string sorted = new string(chars);
+
+            if(!anagrams.ContainsKey(sorted))
+                anagrams[sorted] = new List<string>{};
+
+            anagrams[sorted].Add(str);
+        }
+
+        return anagrams.Values.ToList();
+    }
+}
