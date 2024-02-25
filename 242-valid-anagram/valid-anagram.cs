cb65f291ab64b1f71cbcde2093505a0d7e4f410b
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-25 12:43:15 +0400

    Time: 116 ms (10.83%) | Memory: 44.9 MB (7.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,14 +1,6 @@
 public class Solution {
     public bool IsAnagram(string s, string t) 
     {  
-        char[] firstChars = s.ToCharArray();
-        Array.Sort(firstChars);
-        string firstSorted = new string(firstChars);
-
-        char[] secondChars = t.ToCharArray();
-        Array.Sort(secondChars);
-        string secondSorted = new string(secondChars);
-
-        return firstSorted == secondSorted;
+        return s.OrderBy(c => c).SequenceEqual(t.OrderBy(c => c));
     }
 }
