381c5b2ac491ec077768505ccf783d5c48c77a96
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-25 08:35:01 +0400

    Time: 360 ms (5.06%) | Memory: 48.8 MB (6.57%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var value = target - nums[i];
+
+            if (dict.ContainsValue(value))
+                return new int[] { dict.Where(x=>x.Value == value).First().Key, i };
+            else
+                dict.Add(i, nums[i]);
+        }
+
+        return null;
+    }
+}
