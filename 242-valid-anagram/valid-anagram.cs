74ed578ff2f38e828e5ce67cd3af62ad2d7112c4
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-25 12:42:03 +0400

    Time: 76 ms (20.54%) | Memory: 42.1 MB (57.50%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,6 +1,27 @@
 public class Solution {
     public bool IsAnagram(string s, string t) 
     {  
-        return s.OrderBy(c => c).SequenceEqual(t.OrderBy(c => c));
+        var dict = new Dictionary<char, int>();
+
+        foreach(var c in s)
+        {
+            if(dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict.Add(c, 1);
+        }
+
+        foreach(var c in t)
+        {
+            if(dict.ContainsKey(c))
+                dict[c]--;
+            else
+                return false;
+        }
+
+        if(dict.Values.All(v => v == 0))
+            return true;
+        else
+            return false;
     }
 }
