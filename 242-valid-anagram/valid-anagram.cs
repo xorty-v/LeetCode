8b4a16e45a1e2ae7929878dab55f87e2cabb4ca6
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-25 09:26:18 +0400

    Time: 59 ms (70.79%) | Memory: 42.9 MB (19.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,33 +1,14 @@
 public class Solution {
     public bool IsAnagram(string s, string t) 
-    {
-        if(s == t)
-            return true;
-        
-        if(s.Length != t.Length)
-            return false;
-        
-        var dict = new Dictionary<char, int>();
+    {  
+        char[] firstChars = s.ToCharArray();
+        Array.Sort(firstChars);
+        string firstSorted = new string(firstChars);
 
-        foreach(var c in s)
-        {
-            if(dict.ContainsKey(c))
-                dict[c]++;
-            else
-                dict.Add(c, 1);
-        }
+        char[] secondChars = t.ToCharArray();
+        Array.Sort(secondChars);
+        string secondSorted = new string(secondChars);
 
-        foreach(var c in t)
-        {
-            if(dict.ContainsKey(c))
-                dict[c]--;
-            else
-                return false;
-        }
-
-        if(dict.Values.All(v => v == 0))
-            return true;
-        else
-            return false;
+        return firstSorted == secondSorted;
     }
 }
