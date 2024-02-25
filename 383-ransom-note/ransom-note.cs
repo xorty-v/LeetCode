06e03f36b15b000e8be744b970d3ab08b6db7a4b
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-25 11:34:08 +0400

    Time: 55 ms (90.94%) | Memory: 44 MB (91.24%) - LeetSync

--- 383-ransom-note/ransom-note.cs
+++ 383-ransom-note/ransom-note.cs
@@ -1,14 +1,16 @@
 public class Solution {
-    public bool CanConstruct(string ransomNote, string magazine) {
+    public bool CanConstruct(string ransomNote, string magazine) 
+    {
         var list = new List<char>(ransomNote);
+
         int count = 0;
 
         foreach(var c in magazine)
         {
             if(list.Remove(c))
                 count++;
-        }
+        }  
 
         return ransomNote.Length == count;
-    }
+    }   
 }
