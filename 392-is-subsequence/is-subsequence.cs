237ce3c2b12451c6868c2e1906154a0b66774cf6
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-02-29 12:02:09 +0400

    Time: 61 ms (23.33%) | Memory: 39.5 MB (86.92%) - LeetSync

--- 392-is-subsequence/is-subsequence.cs
+++ 392-is-subsequence/is-subsequence.cs
@@ -2,14 +2,11 @@ public class Solution {
     public bool IsSubsequence(string s, string t) 
     {
         int sIndex = 0;
-        int tIndex = 0;
 
-        while(sIndex < s.Length && tIndex < t.Length)
+        for(int tIndex = 0; tIndex < t.Length && sIndex < s.Length ; tIndex++)
         {
-            if (s[sIndex] == t[tIndex])
+            if(s[sIndex] == t[tIndex])
                 sIndex++;
-
-            tIndex++;
         }
 
         return s.Length == sIndex;
