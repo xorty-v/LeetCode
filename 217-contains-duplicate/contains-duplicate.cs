dae30567c271036e546e45671687ac4c394974b0
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2024-09-28 20:03:21 +0400

    Time: 269 ms (50.61%) | Memory: 55.2 MB (92.74%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -1,16 +1,6 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) 
     {
-        HashSet<int> hashSet = new HashSet<int>();
-
-        foreach(var num in nums) 
-        {
-            if(hashSet.Contains(num))
-               return true;
-
-            hashSet.Add(num);
-        }
-
-        return false;
+        return new HashSet<int>(nums).Count < nums.Length;
     }
 }
