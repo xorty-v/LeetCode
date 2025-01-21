148351a892deffc15e463a230cc26702b74f91a3
Ramazi Gvindzhiliya <ramazi.gvindzhiliya@gmail.com>
2025-01-22 01:30:38 +0400

    Time: 45 ms (66.54%) | Memory: 73.3 MB (12.81%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) 
     {
-        HashSet<int> numSet = new HashSet<int>(nums);
+        var numSet = new HashSet<int>(nums);
         int longest = 0;
 
-        foreach (int num in numSet) {
-            if (!numSet.Contains(num - 1)) {
-                int length = 1;
-                while (numSet.Contains(num + length)) {
-                    length++;
+        foreach(var num in numSet)
+        {
+            if(!numSet.Contains(num - 1))
+            {
+                int counter = 1;
+                while(numSet.Contains(num + counter))
+                {
+                    counter++;
                 }
-                longest = Math.Max(longest, length);
-            }
+
+                longest = Math.Max(longest, counter);
+            }   
         }
-        
-        return longest;    
+
+        return longest;
     }
 }
